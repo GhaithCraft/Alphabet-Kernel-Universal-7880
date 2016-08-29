de7c8dd3f6df08a40e5a244e5dd5582c020e0655
Tkkg1994 <lg@linux.com>
2016-08-29 14:08:21 +0200

    selinux: Allow SELinux to be Permissive or Enforcing
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- security/selinux_m/exports.c
+++ security/selinux_m/exports.c
@@ -19,14 +19,20 @@
 
 bool selinux_is_enabled(void)
 {
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+	return true;
+#else
 	return selinux_enabled;
+#endif
 }
 EXPORT_SYMBOL_GPL(selinux_is_enabled);
 
 bool selinux_is_enforcing(void)
 {
-#ifdef CONFIG_ALWAYS_ENFORCE
+#if defined(CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE)
 	return true;
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+	return false;
 #else
 	return selinux_enforcing;
 #endif
