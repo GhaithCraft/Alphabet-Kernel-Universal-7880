7e95edff2732855429499dfb16e81d01a83c0f7c
Tkkg1994 <luca.grifo@outlook.com>
2017-01-22 20:11:12 +0100

    Permissive SELinux Patch
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -1046,10 +1046,12 @@ static noinline int avc_denied(u32 ssid, u32 tsid,
 	}
 #endif
 
-#ifdef CONFIG_ALWAYS_ENFORCE
+#if defined(CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE)
 	if (!(avd->flags & AVD_FLAGS_PERMISSIVE))
-#else
+		return -EACCES;
+#elif !defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
 	if (selinux_enforcing && !(avd->flags & AVD_FLAGS_PERMISSIVE))
+		return -EACCES;
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
 
