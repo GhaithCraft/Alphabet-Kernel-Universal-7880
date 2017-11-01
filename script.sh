90a41db908e970be60b1a53efb5048b2206bb2b5
GhaithCraft <GhaithCraft@Gmail.com>
2017-11-01 16:42:46 +0300

    use GCC 6.4.1 Toolchain (built with Crosstool-NG) (A custom built toolchain by @djb77)

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Cleanup before build
 
-echo "Compile wtih linaro Toolchain"
+echo "Compile wtih GCC 6.4.1 Toolchain"
 export CROSS_COMPILE=/home/ghaith/android/Toolchains/aarch64-cortex_a53-linux-gnueabi-gcc-6/bin/aarch64-cortex_a53-linux-gnueabi-
 #export CROSS_COMPILE=/home/ghaith/android/Toolchains/gcc-linaro-7.1.1-2017.08-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 ARCH=arm64
